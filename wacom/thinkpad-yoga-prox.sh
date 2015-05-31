f1827a8737d9646f907f92ec2d28ed0ab9080fd5
sjjh <simon.harhues@muenster.de>
2015-05-31 12:53:20 +0200

    add new touch screen device for tp Yoga 12. This fixes #9

--- wacom/thinkpad-yoga-prox.sh
+++ wacom/thinkpad-yoga-prox.sh
@@ -5,6 +5,14 @@
 
 sleeptime="0.3s"
 lastPenPosition="Proximity=Out"
+
+if xinput --list | grep -q "ELAN Touchscreen"
+then
+    touchscreen="ELAN Touchscreen"
+else
+    touchscreen="SYNAPTICS Synaptics Touch Digitizer V04"
+fi
+
 while true
 do 
     mssg="`xinput query-state "Wacom ISDv4 EC Pen stylus" | grep Proximity`"
@@ -15,14 +23,14 @@ do
             "Proximity=In" )
                 if [ $lastPenPosition != $inout ]
                 then
-                    xinput --disable "ELAN Touchscreen"
+                    xinput --disable "$touchscreen"
                     lastPenPosition=$inout
                 fi
                 ;;
             "Proximity=Out" )
                 if [ $lastPenPosition != $inout ]
                 then
-                    xinput --enable "ELAN Touchscreen"
+                    xinput --enable "$touchscreen"
                     lastPenPosition=$inout
                 fi
                 ;;
